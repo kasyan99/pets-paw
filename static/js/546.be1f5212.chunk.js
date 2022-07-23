335d7f222a83cd47e91a6e87f0bbdfb165bf13f8
Andrey <kasyan201999@gmail.com>
2022-07-23 12:53:42 +0300

    Updates

---
+++ static/js/546.be1f5212.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpets_paw=self.webpackChunkpets_paw||[]).push([[546],{1879:function(a,e,c){c.r(e),c.d(e,{default:function(){return u}});c(2791);var t="HomePage_homePage__PvHPg",n="HomePage_black__NVf08",s=c.p+"static/media/girl-and-pet 1.1927e13f60069098098c.png",r=c(9434),i=c(1633),p=c(184),u=function(){var a=(0,r.v9)(i.v);return(0,p.jsx)("div",{className:"".concat(t," ").concat(a&&n),children:(0,p.jsx)("img",{src:s,alt:"cat and girl"})})}}}]);
+//# sourceMappingURL=546.be1f5212.chunk.js.map
